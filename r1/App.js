5a98913ebafc771ce48efcd381f93f71bc7bb98b
Igor Gritsenko <igor@gritsenko.biz>
2021-12-13 00:57:28 +0300

    canvas play

--- r1/App.js
+++ r1/App.js
@@ -1,9 +1,14 @@
+import { Pix2dApp } from "./core/Pix2dApp.js";
+
 export class App {
 
     static instace = new App();
 
     constructor(){
         console.log("Pix2d loading...")
+
+        const cnavasContainer = document.getElementById("canvas-container");
+        this.Pix2dApp = new Pix2dApp(cnavasContainer);
     }
 }
 
