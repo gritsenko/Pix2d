5fc9a8e5eabc99c537be4d0162413d300b03a616
Igor Gritsenko <igor@gritsenko.biz>
2023-07-20 00:42:54 +0300

    Updated android head

--- Sources/PlatformHeads/Pix2d.Android/MainActivity.cs
+++ Sources/PlatformHeads/Pix2d.Android/MainActivity.cs
@@ -4,23 +4,40 @@ using System.Threading.Tasks;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Avalonia;
 using Avalonia.Android;
 
 namespace Pix2d.Android;
 
 [Activity(Label = "Pix2d.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@mipmap/ic_launcher", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
-public class MainActivity : AvaloniaMainActivity {
-    internal static MainActivity Instance { get; private set; }
+//[Activity(
+//    Label = "AvaloniaTest.Android",
+//    Theme = "@style/MyTheme.NoActionBar",
+//    Icon = "@drawable/icon",
+//    MainLauncher = true,
+//    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 
-    protected override void OnCreate(Bundle savedInstanceState) {
+public class MainActivity : AvaloniaMainActivity<EditorApp>
+{
+    internal static MainActivity Instance { get; private set; }
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont();
+    }
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
         Instance = this;
 
         base.OnCreate(savedInstanceState);
         Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+        
         // Set our view from the "main" layout resource
         var app = (EditorApp)Avalonia.Application.Current;
         Content = (app)?.HostView;
         app.UpdateTopLevelFromHostView();
+        
         AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
         TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
 
@@ -32,18 +49,22 @@ public class MainActivity : AvaloniaMainActivity {
         base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    private static void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs) {
+    private static void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs)
+    {
         var newExc = new Exception("TaskSchedulerOnUnobservedTaskException", unobservedTaskExceptionEventArgs.Exception);
         LogUnhandledException(newExc);
     }
 
-    private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs) {
+    private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+    {
         var newExc = new Exception("CurrentDomainOnUnhandledException", unhandledExceptionEventArgs.ExceptionObject as Exception);
         LogUnhandledException(newExc);
     }
 
-    internal static void LogUnhandledException(Exception exception) {
-        try {
+    internal static void LogUnhandledException(Exception exception)
+    {
+        try
+        {
             const string errorFileName = "Fatal.log";
             var libraryPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); // iOS: Environment.SpecialFolder.Resources
             var errorFilePath = Path.Combine(libraryPath, errorFileName);
@@ -54,8 +75,61 @@ public class MainActivity : AvaloniaMainActivity {
             // Log to Android Device Logging.
             //Android.Util.Log.Error("Crash Report", errorMessage);
         }
-        catch {
+        catch
+        {
             // just suppress any error logging exceptions
         }
     }
+
 }
+
+//public class MainActivity : AvaloniaMainActivity {
+//    internal static MainActivity Instance { get; private set; }
+
+//    protected override void OnCreate(Bundle savedInstanceState) {
+//        Instance = this;
+
+//        base.OnCreate(savedInstanceState);
+//        Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+//        // Set our view from the "main" layout resource
+//        var app = (EditorApp)Avalonia.Application.Current;
+//        Content = (app)?.HostView;
+//        app.UpdateTopLevelFromHostView();
+//        AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+//        TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+
+//    }
+
+//    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+//    {
+//        Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+//        base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+//    }
+
+//    private static void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs) {
+//        var newExc = new Exception("TaskSchedulerOnUnobservedTaskException", unobservedTaskExceptionEventArgs.Exception);
+//        LogUnhandledException(newExc);
+//    }
+
+//    private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs) {
+//        var newExc = new Exception("CurrentDomainOnUnhandledException", unhandledExceptionEventArgs.ExceptionObject as Exception);
+//        LogUnhandledException(newExc);
+//    }
+
+//    internal static void LogUnhandledException(Exception exception) {
+//        try {
+//            const string errorFileName = "Fatal.log";
+//            var libraryPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); // iOS: Environment.SpecialFolder.Resources
+//            var errorFilePath = Path.Combine(libraryPath, errorFileName);
+//            var errorMessage = String.Format("Time: {0}\r\nError: Unhandled Exception\r\n{1}",
+//                DateTime.Now, exception.ToString());
+//            File.WriteAllText(errorFilePath, errorMessage);
+
+//            // Log to Android Device Logging.
+//            //Android.Util.Log.Error("Crash Report", errorMessage);
+//        }
+//        catch {
+//            // just suppress any error logging exceptions
+//        }
+//    }
+//}
