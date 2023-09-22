d339a644cb8209f99bac2eafbe2cda14b017286c
Igor Gritsenko <igor@gritsenko.biz>
2023-09-23 00:04:09 +0300

    windows store stuff

--- Sources/Pix2d.Desktop.Shared/DesktopPix2dBootstrapper.cs
+++ Sources/Pix2d.Desktop.Shared/DesktopPix2dBootstrapper.cs
@@ -88,7 +88,7 @@ public class DesktopPix2dBootstrapper : IPix2dBootstrapper
         Debug.WriteLine("Register appcenter for winforms");
         AppCenter.Start("2c0dc23b-1bcd-42dc-b7c2-d6944fab2c58", typeof(Analytics), typeof(Crashes));
         Logger.RegisterLoggerTarget(new AppCenterLoggerTarget());
-#else
+#elif !WINDOWS_UWP
         Logger.RegisterLoggerTarget(new SentryLoggerTarget());
 #endif
 
