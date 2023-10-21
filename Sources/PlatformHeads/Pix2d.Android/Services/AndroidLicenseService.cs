17dc0db0bd1720589eb104baff1e563d4ae4f673
Igor Gritsenko <igor@gritsenko.biz>
2023-10-22 00:53:51 +0300

    Added license service for android

---
+++ Sources/PlatformHeads/Pix2d.Android/Services/AndroidLicenseService.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonServiceLocator;
+using Pix2d.Abstract.Services;
+using Plugin.InAppBilling;
+
+namespace Pix2d.Services
+{
+    public class AndroidLicenseService : ILicenseService, IInAppBillingVerifyPurchase
+    {
+        public IDialogService DialogService { get; }
+
+        public ISettingsService SettingsService => CoreServices.SettingsService;
+
+        public event EventHandler LicenseChanged;
+        public string FormattedPrice { get; set; } = $"9.99";
+
+        public bool AllowBuyPro { get; } = true;
+        public bool IsPro { get; set; } = false;
+
+        public AndroidLicenseService(IDialogService dialogService)
+        {
+            DialogService = dialogService;
+
+            Init();
+        }
+
+        public async void Init()
+        {
+
+            try
+            {
+                //#if DEBUG
+                Logger.Log("Checking Pro version and price");
+                await CheckIsPro();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+        }
+
+        public async Task<bool> CheckIsPro(bool proposeUpgrade = true)
+        {
+            var billing = CrossInAppBilling.Current;
+            try
+            {
+
+                var productIds = new string[] { "pix2d_pro" };
+                //You must connect
+                var connected = await billing.ConnectAsync(true);
+
+                if (!connected)
+                {
+                    //Couldn't connect
+                    return false;
+                }
+
+                var proSetting = SettingsService.Get<bool>("IsProActivated");
+
+                var isPro = false;
+
+                if (proSetting != null)
+                {
+                    Logger.Log("initialized Pro from settings");
+                    isPro = true;
+                }
+
+                var purchases = await billing.GetPurchasesAsync(ItemType.InAppPurchase);
+
+                //check for null just incase
+                if (purchases?.Any(p => p.ProductId == productIds[0]) ?? false)
+                {
+                    //Purchase restored
+                    Logger.Log("initialized Pro from license information");
+                    isPro = true;
+                }
+                else
+                {
+                    //no purchases found
+                    isPro = false;
+                }
+
+                if (isPro)
+                {
+
+                    //save to app settings
+                    SettingsService.Set("IsProActivated", true);
+                    IsPro = true;
+                    OnLicenseChanged();
+                }
+
+                if (!isPro)
+                {
+                    var items = await billing.GetProductInfoAsync(ItemType.InAppPurchase, productIds);
+                    var item = items.FirstOrDefault();
+                    FormattedPrice = item.LocalizedPrice;
+                }
+
+                return isPro;
+            }
+            catch (InAppBillingPurchaseException pEx)
+            {
+                HandleCheckProException(pEx);
+            }
+            catch (Exception ex)
+            {
+                HandleCheckProException(ex);
+            }
+            finally
+            {
+                Pix2DApp.Instance.CurrentLicense = IsPro ? "pro" : "free";
+                await billing.DisconnectAsync();
+            }
+
+            return false;
+        }
+
+        private void HandleCheckProException(Exception ex)
+        {
+            Logger.LogException(ex);
+            var dlgService = ServiceLocator.Current.GetInstance<IDialogService>();
+            dlgService?.Alert("Can't get license information from Windows Store. Error: " + ex.Message + ". Please try to restart Pix2d. Contact to the developer, if this problem still persist.", "Getting license error");
+        }
+
+        public void OnLicenseChanged()
+        {
+            LicenseChanged?.Invoke(null, EventArgs.Empty);
+        }
+
+        public async Task<bool> BuyPro()
+        {
+
+            try
+            {
+                var productId = "pix2d_pro";
+
+                var connected = await CrossInAppBilling.Current.ConnectAsync();
+
+                if (!connected)
+                {
+                    //Couldn't connect to billing, could be offline, alert user
+                    DialogService.Alert("Can't connect to google play services.", "Network problem");
+                    return false;
+                }
+
+                //try to purchase item
+                var purchase = await CrossInAppBilling.Current.PurchaseAsync(productId, ItemType.InAppPurchase);
+                if (purchase == null)
+                {
+
+                    Logger.LogEvent($"$ Buy canceled: purchase == null");
+
+                    //Not purchased, alert the user
+                }
+                else
+                {
+                    //Purchased, save this information
+                    var id = purchase.Id;
+                    var token = purchase.PurchaseToken;
+                    var state = purchase.State;
+
+                    SettingsService.Set("purchaseToken", token);
+                    SettingsService.Set("purchaseId", id);
+                    SettingsService.Set("purchaseState", state.ToString());
+
+                    if (state == PurchaseState.Purchased
+                        || state == PurchaseState.Restored
+                        || state == PurchaseState.Purchasing)
+                    {
+                        IsPro = true;
+                        SettingsService.Set("IsProActivated", true);
+                        Logger.LogEvent($"$ Pro version bought: {state}");
+
+                        if (state == PurchaseState.Purchased)
+                        {
+                            var result =
+                                await CrossInAppBilling.Current.FinalizePurchaseOfProductAsync(new[] { productId });
+                            if (result.FirstOrDefault().Success)
+                            {
+                                Logger.LogEvent($"$ Purchase acknowledged");
+                            }
+                        }
+
+                        OnLicenseChanged();
+                        return true;
+                    }
+                    else
+                    {
+                        Logger.LogEvent($"$ Not purchased: {state}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogException(e);
+                Logger.LogEvent($"$ Exception while buying:\n{e.Message}\n{e.StackTrace}");
+
+                //Something bad has occurred, alert user
+            }
+            finally
+            {
+                //Disconnect, it is okay if we never connected
+                await CrossInAppBilling.Current.DisconnectAsync();
+            }
+
+            Logger.LogEvent($"$ Buy cancelled: unknown reason");
+
+            return false;
+        }
+
+        //void handlePurchase(Purchase purchase)
+        //{
+        //    if (purchase.getPurchaseState() == PurchaseState.PURCHASED)
+        //    {
+        //        if (!purchase.isAcknowledged())
+        //        {
+        //            AcknowledgePurchaseParams acknowledgePurchaseParams =
+        //                AcknowledgePurchaseParams.newBuilder()
+        //                    .setPurchaseToken(purchase.getPurchaseToken())
+        //                    .build();
+        //            client.acknowledgePurchase(acknowledgePurchaseParams, acknowledgePurchaseResponseListener);
+        //        }
+        //    }
+        //}
+        public void ToggleIsPro()
+        {
+            IsPro = !IsPro;
+
+            Logger.Log("$On license changed to " + (IsPro ? "PRO" : "ESS"));
+            OnLicenseChanged();
+        }
+
+        public async Task<bool> RateApp()
+        {
+            try
+            {
+                var appId = "com.pix2d.pix2dapp";
+
+                var isInAppReviewShown = SettingsService.Get<bool>("isInAppReviewShown");
+
+                if (isInAppReviewShown != null)
+                {
+                    Plugin.StoreReview.CrossStoreReview.Current.OpenStoreReviewPage(appId);
+                }
+                else
+                {
+                    SettingsService.Set("isInAppReviewShown", true);
+                    await Plugin.StoreReview.CrossStoreReview.Current.RequestReview(false);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+
+            return false;
+        }
+
+        public async Task<bool> VerifyPurchase(string signedData, string signature, string productId = null, string transactionId = null)
+        {
+            return true;
+        }
+    }
+}
--- Sources/PlatformHeads/Pix2d.Android/Services/AndroidPlatformStuffService.cs
+++ Sources/PlatformHeads/Pix2d.Android/Services/AndroidPlatformStuffService.cs
@@ -11,132 +11,131 @@ using Pix2d.Abstract.Export;
 using Pix2d.Android;
 using File = Java.IO.File;
 
-namespace Pix2d.Services
+namespace Pix2d.Services;
+
+public class AndroidPlatformStuffService : IPlatformStuffService
 {
-    public class AndroidPlatformStuffService : IPlatformStuffService
+    private DeviceFormFactorType _deviceFormFactorType = DeviceFormFactorType.Desktop;
+    private string _appVersion;
+
+    public AndroidPlatformStuffService()
     {
-        private DeviceFormFactorType _deviceFormFactorType = DeviceFormFactorType.Desktop;
-        private string _appVersion;
-
-        public AndroidPlatformStuffService()
+        var diag = GetScreenDiagonal();
+        if (diag < 7)
         {
-            var diag = GetScreenDiagonal();
-            if (diag < 7)
-            {
-                _deviceFormFactorType = DeviceFormFactorType.Phone;
-            }
-            else if (diag < 12)
-            {
-                _deviceFormFactorType = DeviceFormFactorType.Tablet;
-            }
-
+            _deviceFormFactorType = DeviceFormFactorType.Phone;
+        }
+        else if (diag < 12)
+        {
+            _deviceFormFactorType = DeviceFormFactorType.Tablet;
         }
 
-        public void OpenUrlInBrowser(string url)
-        {
-            var uri = global::Android.Net.Uri.Parse(url);
-            var intent = new Intent(Intent.ActionView, uri);
-            MainActivity.Instance.StartActivity(intent);
-        }
+    }
 
-        public DeviceFormFactorType GetDeviceFormFactorType()
-        {
-            return _deviceFormFactorType;
-        }
+    public void OpenUrlInBrowser(string url)
+    {
+        var uri = global::Android.Net.Uri.Parse(url);
+        var intent = new Intent(Intent.ActionView, uri);
+        MainActivity.Instance.StartActivity(intent);
+    }
 
-        public void SetWindowTitle(string title)
-        {
+    public DeviceFormFactorType GetDeviceFormFactorType()
+    {
+        return _deviceFormFactorType;
+    }
 
-        }
+    public void SetWindowTitle(string title)
+    {
 
-        public MemoryInfo GetMemoryInfo()
-        {
-            return new MemoryInfo(0, 0);
-        }
+    }
 
-        public bool Is64App { get; }
-        public string KeyToString(VirtualKeys key)
-        {
-            return key.ToString();
-        }
+    public MemoryInfo GetMemoryInfo()
+    {
+        return new MemoryInfo(0, 0);
+    }
 
-        public string GetAppVersion()
-        {
-            //string GetAppVer()
-            //{
-            //    var package = Package.Current;
-            //    var packageId = package.Id;
-            //    var version = packageId.Version;
+    public bool Is64App { get; }
+    public string KeyToString(VirtualKeys key)
+    {
+        return key.ToString();
+    }
 
-            //    return $"{version.Major}.{version.Minor}";
-            //}
+    public string GetAppVersion()
+    {
+        //string GetAppVer()
+        //{
+        //    var package = Package.Current;
+        //    var packageId = package.Id;
+        //    var version = packageId.Version;
 
-            //return _appVersion ?? (_appVersion = GetAppVer());
-            return "Alpha";
-        }
+        //    return $"{version.Major}.{version.Minor}";
+        //}
 
-        public void ToggleTopmostWindow()
-        {
-            throw new NotImplementedException();
-        }
+        //return _appVersion ?? (_appVersion = GetAppVer());
+        return "Alpha";
+    }
 
-        public bool HasKeyboard => false;
-        public bool CanShare => true;
+    public void ToggleTopmostWindow()
+    {
+        throw new NotImplementedException();
+    }
 
-        public static double GetScreenDiagonal()
-        {
-            //try
-            //{
-            //    var display = ((Activity)MainActivity.Instance)?.WindowManager?.DefaultDisplay;
-            //    if (display == null)
-            //        return -1;
+    public bool HasKeyboard => false;
+    public bool CanShare => true;
 
-            //    var displayMetrics = new DisplayMetrics();
-            //    display.GetMetrics(displayMetrics);
+    public static double GetScreenDiagonal()
+    {
+        //try
+        //{
+        //    var display = ((Activity)MainActivity.Instance)?.WindowManager?.DefaultDisplay;
+        //    if (display == null)
+        //        return -1;
 
-            //    if (displayMetrics == null)
-            //        return -1;
+        //    var displayMetrics = new DisplayMetrics();
+        //    display.GetMetrics(displayMetrics);
 
-            //    var wInches = displayMetrics.WidthPixels / (double)displayMetrics.DensityDpi;
-            //    var hInches = displayMetrics.HeightPixels / (double)displayMetrics.DensityDpi;
+        //    if (displayMetrics == null)
+        //        return -1;
 
-            //    var screenDiagonal = Math.Sqrt(Math.Pow(wInches, 2) + Math.Pow(hInches, 2));
-            //    return screenDiagonal;
-            //}
-            //catch (Exception e)
-            //{
-            //    Logger.LogException(e);
-            //}
+        //    var wInches = displayMetrics.WidthPixels / (double)displayMetrics.DensityDpi;
+        //    var hInches = displayMetrics.HeightPixels / (double)displayMetrics.DensityDpi;
+
+        //    var screenDiagonal = Math.Sqrt(Math.Pow(wInches, 2) + Math.Pow(hInches, 2));
+        //    return screenDiagonal;
+        //}
+        //catch (Exception e)
+        //{
+        //    Logger.LogException(e);
+        //}
             
-            return -1;
-        }
+        return -1;
+    }
 
-        public async void Share(IStreamExporter exporter, double scale)
+    public async void Share(IStreamExporter exporter, double scale)
+    {
+        var tempFilename = "pix2d_share" + exporter.SupportedExtensions.First();
+        var sdCardPath = Path.Combine(Application.Context.ExternalCacheDir.AbsolutePath, "tmp");
+        if (!Directory.Exists(sdCardPath))
         {
-            var tempFilename = "pix2d_share" + exporter.SupportedExtensions.First();
-            var sdCardPath = Path.Combine(Application.Context.ExternalCacheDir.AbsolutePath, "tmp");
-            if (!Directory.Exists(sdCardPath))
-            {
-                Directory.CreateDirectory(sdCardPath);
-            }
-            
-            var filePath = Path.Combine(sdCardPath, tempFilename);
-            using (var os = new FileStream(filePath, FileMode.Create))
-            {
-                var nodes = CoreServices.ExportService.GetNodesToExport(scale);
-                var source = await exporter.ExportToStreamAsync(nodes, scale);
-                await source.CopyToAsync(os);
-                os.Close();
-            }
-
-            var imageUri = FileProvider.GetUriForFile(Application.Context, Application.Context.PackageName + ".fileprovider",
-                new File(filePath));
-            var sharingIntent = new Intent ();
-            sharingIntent.SetAction (Intent.ActionSend);
-            sharingIntent.SetType(exporter.MimeType);
-            sharingIntent.PutExtra (Intent.ExtraStream, imageUri);
-            sharingIntent.AddFlags (ActivityFlags.GrantReadUriPermission);
-            MainActivity.Instance.StartActivity (Intent.CreateChooser (sharingIntent, "Pix2d project"));
+            Directory.CreateDirectory(sdCardPath);
         }
+            
+        var filePath = Path.Combine(sdCardPath, tempFilename);
+        using (var os = new FileStream(filePath, FileMode.Create))
+        {
+            var nodes = CoreServices.ExportService.GetNodesToExport(scale);
+            var source = await exporter.ExportToStreamAsync(nodes, scale);
+            await source.CopyToAsync(os);
+            os.Close();
+        }
+
+        var imageUri = FileProvider.GetUriForFile(Application.Context, Application.Context.PackageName + ".fileprovider",
+            new File(filePath));
+        var sharingIntent = new Intent ();
+        sharingIntent.SetAction (Intent.ActionSend);
+        sharingIntent.SetType(exporter.MimeType);
+        sharingIntent.PutExtra (Intent.ExtraStream, imageUri);
+        sharingIntent.AddFlags (ActivityFlags.GrantReadUriPermission);
+        MainActivity.Instance.StartActivity (Intent.CreateChooser (sharingIntent, "Pix2d project"));
     }
 }
