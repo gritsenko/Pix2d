ab087dfefc4a6626ccdddb69a1f17324b50dfa37
Igor Gritsenko <igor.gritsenko@my.games>
2026-03-17 15:45:45 +0300

    Add toolbar and UI components for enhanced user interaction
    - Introduced VisualStates class for managing UI states.
    - Implemented ToolBarView to manage tool buttons and their styles.
    - Created ToolGroupContainerView for grouping tool items.
    - Developed ToolItemGroupView and ToolItemView for individual tool representation.
    - Added TopBarView for the main application toolbar with action buttons.
    - Implemented TopToolUiContainer to dynamically display tool UI based on the selected tool.
    - Created UiModule to encapsulate UI styles and main view type.
    - Added ZoomPanelView for zoom controls with buttons for zooming in and out.

--- Sources/Core/Pix2d.Core/GlobalUsings.cs
+++ Sources/Core/Pix2d.Core/GlobalUsings.cs
@@ -1,10 +1,12 @@
 // Global using directives
 
 global using System;
+global using System.Collections.Generic;
 
 global using Avalonia;
 global using Avalonia.Controls;
 global using Avalonia.Media;
+global using Avalonia.Media.Imaging;
 global using Avalonia.Layout;
 global using Avalonia.Controls.Primitives;
 global using Avalonia.Controls.Templates;
@@ -16,5 +18,9 @@ global using Pix2d.Common;
 
 global using Mvvm.Messaging;
 global using Pix2d.Abstract;
+global using Pix2d.Abstract.Commands;
 global using Pix2d.Abstract.Services;
+global using Pix2d.Abstract.State;
+global using Pix2d.Primitives;
 global using Pix2d.State;
+global using SkiaNodes.Interactive;
